26039aebd242d8bdc07a5fb9136fae68c3f4e0f6
milton.stanley@gmail.com <milton.stanley@gmail.com>
2012-10-25 21:15:33 -0500

    Creating real one now

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrderServer::Application.config.secret_token = '650944b862479e33ea84a813a65b8af26f8517986d67442679c2d210ec2678743269c2d30f644cc2850b59d1876d7aff08b6fac89da96fb8b0630e63f548d826'
