7b82afae99e65899423709b85ff96563b90e432c
MiltonStanley <milton.stanley@gmail.com>
2012-10-31 10:58:17 -0500

    Edited views & model to reflect print_cost field addition

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,3 @@
 class Order < ActiveRecord::Base
-  attr_accessible :additional_design, :base_design, :binding, :business, :coating, :contact, :cutting, :date, :date_promised, :drilling, :employee, :flat_size, :folding, :inkspec, :item, :laminating, :location, :miscellaneous, :numbering, :padding, :paper_color, :paper_weight, :phone, :price, :quantity, :stapling
+  attr_accessible :additional_design, :base_design, :binding, :business, :coating, :contact, :cutting, :date, :date_promised, :drilling, :employee, :flat_size, :folding, :inkspec, :item, :laminating, :location, :miscellaneous, :numbering, :padding, :paper_color, :paper_weight, :phone, :price, :quantity, :stapling, :print_cost
 end
